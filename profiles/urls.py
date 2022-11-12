2b1d3d4e1ec8aad0b3b15f5912e8a89d44070ab4
Jordan <jrdnbrkfld@icloud.com>
2022-11-12 13:32:18 +0000

    formatted documents

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-   path('profiles/', views.ProfileList.as_view()),
+    path('profiles/', views.ProfileList.as_view()),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 
+
 class ProfileList(APIView):
-   def get(self, request):
-      profiles = Profile.objects.all()
-      serializer = ProfileSerializer(profiles, many=True)
-      return Response(serializer.data)
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
