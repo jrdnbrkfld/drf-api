a1e87bf53876f75f40a3cc296ad6581199e58feb
Jordan <jrdnbrkfld@icloud.com>
2022-11-28 09:27:23 +0000

    fixed post/followers/following count issue

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Count
 from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -18,12 +17,7 @@ class ProfileList(generics.ListAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-    filterset_fields = [
-        'owner__following__followed__profile',
-        'owner__followed__owner__profile',
+        filters.OrderingFilter
     ]
     ordering_fields = [
         'posts_count',
